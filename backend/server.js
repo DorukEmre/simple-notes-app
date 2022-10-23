424d5ef45c467c51e1d0a67e786c14e7ea29a7ac
DorukEmre <doruk.emre@outlook.com>
2022-10-23 16:49:15 +0100

    Revert "Set up for build"
    This reverts commit 9a70728fcedb8d4df3e5fb535c31804edbf98af1.

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
-const path = require('path')
-const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
@@ -12,18 +10,9 @@ app.use(express.json())
 // connect to mongoose
 mongoose.connect(process.env.DB_STRING)
 
-// If we are in production environment, use static client files, and use main 'index.html' file
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
-  // General route applicable to any URL
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
-  })
-}
-
 // routes
 app.use('/', require('./routes/note.route'))
 
-app.listen(PORT, () => {
-  console.log(`express server on port ${PORT}, http://localhost:${PORT}/`)
+app.listen(3001, () => {
+  console.log('express server running on port 3001, http://localhost:3001/')
 })
