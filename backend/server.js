9a70728fcedb8d4df3e5fb535c31804edbf98af1
DorukEmre <doruk.emre@outlook.com>
2022-10-23 12:28:33 +0100

    Set up for build

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const path = require('path')
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +15,15 @@ mongoose.connect(process.env.DB_STRING)
 // routes
 app.use('/', require('./routes/note.route'))
 
-app.listen(3001, () => {
-  console.log('express server running on port 3001, http://localhost:3001/')
+// If we are in production environment, use static client files, and use main 'index.html' file
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
+  // General route applicable to any URL
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+  })
+}
+
+app.listen(PORT, () => {
+  console.log(`express server on port ${PORT}, http://localhost:${PORT}/`)
 })
