d182ba66403b8d8be1c5dc2136d0427f9951b770
DorukEmre <doruk.emre@outlook.com>
2022-10-23 14:48:53 +0100

    Logging server path

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'))
   // General route applicable to any URL
   app.get('*', (req, res) => {
+    console.log(__dirname)
+    console.log(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
   })
 }
