6cdf0a6e05b73c6927207dc71e79fc620fdbd035
DorukEmre <doruk.emre@outlook.com>
2022-10-23 16:48:26 +0100

    Revert "Logging server path"
    This reverts commit d182ba66403b8d8be1c5dc2136d0427f9951b770.

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'))
   // General route applicable to any URL
   app.get('*', (req, res) => {
-    console.log(__dirname)
-    console.log(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
   })
 }
