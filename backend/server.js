b52adbd0c41eb852fef44b18ac0e17363c55a84b
DorukEmre <doruk.emre@outlook.com>
2022-10-23 15:25:35 +0100

    Moved routes to end of server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,10 @@ app.use(express.json())
 // connect to mongoose
 mongoose.connect(process.env.DB_STRING)
 
-// routes
-app.use('/', require('./routes/note.route'))
-
 // If we are in production environment, use static client files, and use main 'index.html' file
 if (process.env.NODE_ENV === 'production') {
   console.log('NODE_ENV is ', process.env.NODE_ENV)
+  console.log(path.resolve(__dirname, '../frontend/build', 'index.html'))
   app.use(express.static('frontend/build'))
   // General route applicable to any URL
   app.get('*', (req, res) => {
@@ -26,6 +24,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
   })
 }
+
+// routes
+app.use('/', require('./routes/note.route'))
 
 app.listen(PORT, () => {
   console.log(`express server on port ${PORT}, http://localhost:${PORT}/`)
